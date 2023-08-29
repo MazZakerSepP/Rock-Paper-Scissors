4826ff9280c8d5a4753283a52ebf2f30d93069e9
MazZakerSepP <140405574+MazZakerSepP@users.noreply.github.com>
2023-08-29 14:53:32 +0200

    Update Rock-Paper-Scissors.py

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,29 +1,29 @@
 from random import randint
 
-t = ["Rock", "Paper", "Scissors"]
+t = ["Schere", "Stein", "Papier"]
 computer = t[randint(0,2)]
 player = False
 
 while player == False:
-    player = input("Rock, Paper, Scissors?")
+    player = input("Schere, Stein, Papier?")
     if player == computer:
-        print("Tie!")
-    elif player == "Rock":
-        if computer == "Paper":
-            print("You lose!", computer, "covers", player)
+        print("Unentschieden!")
+    elif player == "Stein":
+        if computer == "Papier":
+            print("Du hast Verloren!", computer, "umwickelt", player)
         else:
-            print("You win!", player, "smashes", computer)
-    elif player == "Paper":
-        if computer == "Scissors":
-            print("You lose!", computer, "cut", player)
+            print("Du hast Gewonnen!", player, "zerstört", computer)
+    elif player == "Papier":
+        if computer == "Schere":
+            print("Du hast Verloren!", computer, "schneidet", player)
         else:
-            print("You win!", player, "covers", computer)
+            print("Du hast Gewonnen!", player, "umwickelt", computer)
     elif player == "Scissors":
         if computer == "Rock":
-            print("You lose...", computer, "smashes", player)
+            print("Du hast Verloren...", computer, "zerstört", player)
         else:
-            print("You win!", player, "cut", computer)
+            print("Du hast Gewonnen!", player, "schneidet", computer)
     else:
-        print("That's not a valid play. Check your spelling!")
+        print("Falsche Dateneingabe. Achte auf die Rechtschreibung!")
     player = False
     computer = t[randint(0,2)]
